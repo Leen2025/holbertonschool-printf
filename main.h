bd5ed6d4f540ccc796026af629556f32da64b879
Leen2025 <leeeen4@hotmail.com>
2025-03-25 05:17:20 +0000

    holbertonschool-printf

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(char *str);
+int print_number(int num);
+#endif
