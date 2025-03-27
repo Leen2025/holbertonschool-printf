c912488684a3e5190b5f713665f797ee42c21fc2
Leen2025 <leeeen4@hotmail.com>
2025-03-27 04:03:25 +0000

    holbertonschool-printf

--- main.h
+++ main.h
@@ -7,4 +7,8 @@ int _putchar(char c);
 int print_string(char *str);
 int print_number(int num);
 int print_binary(unsigned int n);
+int print_unsigned(unsigned int n);
+int print_octal(unsigned int n);
+int print_hex_upper(unsigned int n);
+int print_hex_lower(unsigned int n);
 #endif
