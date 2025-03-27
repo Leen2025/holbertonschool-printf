7cf51e27dca16c2c4eda99494201beaf239f0fcd
Leen2025 <leeeen4@hotmail.com>
2025-03-27 07:07:45 +0000

    holbertonschool-printf

--- functions6.c
+++ functions6.c
@@ -9,7 +9,9 @@ int print_pointer(void *ptr)
 	int count = 0;
 	unsigned long int address;
 	if ( ptr == NULL)
-		return(print_string("(null)");
+	{
+		return(print_string("(null)"));
+	}
 				address = (unsigned long int) ptr;
 				count += _putchar('0');
 				count += _putchar('x');
