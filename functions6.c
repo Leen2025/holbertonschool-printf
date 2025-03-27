bb95b73d57b04478aa79379631c2f90d6a5d5d83
Leen2025 <leeeen4@hotmail.com>
2025-03-27 07:45:23 +0000

    holbertonschool-printf

--- functions6.c
+++ functions6.c
@@ -21,3 +21,20 @@ int print_pointer(void *ptr)
     count += print_hex_lower(address);
     return (count);
 }
+
+/**
+ * print_hex_lower - Helper function to print a number in hexadecimal format
+ * @num: The number to print
+ * Return: Number of characters printed
+ */
+int print_hex_lower(unsigned long int num)
+{
+    int count = 0;
+    char hex_chars[] = "0123456789abcdef";
+
+    if (num / 16)
+        count += print_hex_lower(num / 16);  // Recursively break down the number
+    count += _putchar(hex_chars[num % 16]);  // Print the current hex digit
+
+    return (count);
+}
