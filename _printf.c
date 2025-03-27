72b09eb2ca2624f26c71ab6063a396ad433d4916
Leen2025 <leeeen4@hotmail.com>
2025-03-27 05:00:05 +0000

    holbertonschool-printf

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	int buffer_index = 0;
+	buffer_index = 0;
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
