7b7bd4d4dcfbdfd453469e0cf243d3455977dc9a
Leen2025 <leeeen4@hotmail.com>
2025-03-27 07:42:01 +0000

    holbertonschool-printf

--- functions6.c
+++ functions6.c
@@ -6,15 +6,18 @@
  */
 int print_pointer(void *ptr)
 {
-	int count = 0;
-	unsigned long int address;
-	if ( ptr == NULL)
-	{
-		return(_printf("0x0"));
-	}
-	address = (unsigned long int)ptr;
-				count += _putchar('0');
-				count += _putchar('x');
-				count += print_hex_lower(address);
-				return (count);
-				}
+    int count = 0;
+    unsigned long int address;
+
+
+    if (ptr == NULL)
+    {
+        return (print_string("(nil)"));
+    }
+
+    address = (unsigned long int)ptr;
+    count += _putchar('0');
+    count += _putchar('x');
+    count += print_hex_lower(address);
+    return (count);
+}
