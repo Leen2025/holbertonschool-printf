d6d2ed90d25e4dcd8b50643f2e666f2abd8b9410
Leen2025 <leeeen4@hotmail.com>
2025-03-28 07:15:42 +0000

    holbertonschool-printf

--- _printf.c
+++ _printf.c
@@ -1,99 +1,286 @@
-#include <limits.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+/* Global buffer for storing output */
+int buffer_index = 0;
+char output_buffer[1024];
 
 /**
- * _putchar - writes a character to stdout
- * @c: character to print
- * Return: 1 on success
+ * _putchar - Writes a character to the output buffer.
+ * @c: Character to be printed.
+ * Return: 1 on success.
  */
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+    output_buffer[buffer_index++] = c;
+
+    if (buffer_index == 1024)
+    {
+        write(1, output_buffer, buffer_index);
+        buffer_index = 0;
+    }
+
+    return (1);
 }
 
 /**
- * print_number - prints an integer number
- * @num: the number to print
- * Return: the number of characters printed
+ * flash_buffer - Flushes the buffer to the output.
+ */
+void flash_buffer(void)
+{
+    if (buffer_index > 0)
+    {
+        write(1, output_buffer, buffer_index);
+        buffer_index = 0;
+    }
+}
+
+/**
+ * print_string - Prints a string to the output.
+ * @str: String to be printed.
+ * Return: Number of characters printed.
+ */
+int print_string(char *str)
+{
+    int count = 0;
+
+    if (str == NULL)
+    {
+        str = "(null)";
+    }
+
+    while (*str)
+    {
+        _putchar(*str++);
+        count++;
+    }
+
+    return (count);
+}
+
+/**
+ * print_number - Prints an integer to the output.
+ * @num: Integer to be printed.
+ * Return: Number of characters printed.
  */
 int print_number(int num)
 {
     int count = 0;
+
     if (num < 0)
     {
-        count += _putchar('-');
+        _putchar('-');
         num = -num;
+        count++;
     }
 
     if (num >= 10)
+    {
         count += print_number(num / 10);
-    count += _putchar(num % 10 + '0');
-    return count;
+    }
+
+    _putchar(num % 10 + '0');
+    count++;
+
+    return (count);
 }
 
 /**
- * _printf - custom printf function
- * @format: format string containing format specifiers
- * Return: number of characters printed
+ * print_unsigned - Prints an unsigned integer to the output.
+ * @num: Unsigned integer to be printed.
+ * Return: Number of characters printed.
+ */
+int print_unsigned(unsigned int num)
+{
+    int count = 0;
+
+    if (num >= 10)
+    {
+        count += print_unsigned(num / 10);
+    }
+
+    _putchar(num % 10 + '0');
+    count++;
+
+    return (count);
+}
+
+/**
+ * print_hex_lower - Prints a hexadecimal number in lowercase.
+ * @num: Unsigned integer to be printed in hexadecimal.
+ * Return: Number of characters printed.
+ */
+int print_hex_lower(unsigned int num)
+{
+    int count = 0;
+
+    if (num >= 16)
+    {
+        count += print_hex_lower(num / 16);
+    }
+
+    num = num % 16;
+    if (num < 10)
+    {
+        _putchar(num + '0');
+    }
+    else
+    {
+        _putchar(num - 10 + 'a');
+    }
+
+    count++;
+
+    return (count);
+}
+
+/**
+ * print_hex_upper - Prints a hexadecimal number in uppercase.
+ * @num: Unsigned integer to be printed in hexadecimal.
+ * Return: Number of characters printed.
+ */
+int print_hex_upper(unsigned int num)
+{
+    int count = 0;
+
+    if (num >= 16)
+    {
+        count += print_hex_upper(num / 16);
+    }
+
+    num = num % 16;
+    if (num < 10)
+    {
+        _putchar(num + '0');
+    }
+    else
+    {
+        _putchar(num - 10 + 'A');
+    }
+
+    count++;
+
+    return (count);
+}
+
+/**
+ * print_pointer - Prints a pointer in hexadecimal format.
+ * @ptr: Pointer to be printed.
+ * Return: Number of characters printed.
+ */
+int print_pointer(void *ptr)
+{
+    unsigned long address = (unsigned long)ptr;
+    int count = 0;
+
+    count += _putchar('0');
+    count += _putchar('x');
+    count += print_hex_lower(address);
+
+    return (count);
+}
+
+/**
+ * _printf - Produces output according to a format.
+ * @format: Format string containing format specifiers.
+ * Return: Number of characters printed.
  */
 int _printf(const char *format, ...)
 {
     int count = 0;
     va_list args;
-    const char *ptr = format;
+    int plus_flag, space_flag, hash_flag;
+
+    if (!format || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
 
     va_start(args, format);
 
-    while (*ptr)
+    while (*format)
     {
-        if (*ptr == '%')
+        if (*format == '%')
         {
-            ptr++;
-            if (*ptr == '\0')
+            format++;
+            if (*format == '\0')
                 return (-1);
-            if (*ptr == '%')
-                count += _putchar('%');
-            else if (*ptr == 'd' || *ptr == 'i')
-                count += print_number(va_arg(args, int));
-            else if (*ptr == 'u')
-                count += print_number(va_arg(args, unsigned int));
-            else if (*ptr == 'o')
-                count += print_number(va_arg(args, unsigned int));
-            else if (*ptr == 'x')
-                count += print_number(va_arg(args, unsigned int));
-            else if (*ptr == 'X')
-                count += print_number(va_arg(args, unsigned int));
-            else if (*ptr == 'c')
-                count += _putchar(va_arg(args, int));
-            else if (*ptr == 's')
-                count += _putchar(va_arg(args, char *));
-            else if (*ptr == 'p')
+
+            /* Flags Handling */
+            plus_flag = 0;
+            space_flag = 0;
+            hash_flag = 0;
+            while (*format == '+' || *format == ' ' || *format == '#')
             {
-                unsigned long addr = (unsigned long)va_arg(args, void *);
-                count += _putchar('0');
-                count += _putchar('x');
-                count += print_number(addr);  // You may need to convert the address correctly
+                if (*format == '+')
+                    plus_flag = 1;
+                else if (*format == ' ')
+                    space_flag = 1;
+                else if (*format == '#')
+                    hash_flag = 1;
+                format++;
             }
-            else if (*ptr == 'r')
+
+            /* Handle format specifiers */
+            if (*format == 'c')
+                count += _putchar(va_arg(args, int));
+            else if (*format == 's')
+                count += print_string(va_arg(args, char *));
+            else if (*format == '%')
                 count += _putchar('%');
+            else if (*format == 'd' || *format == 'i')
+            {
+                int num = va_arg(args, int);
+                if (num >= 0)
+                {
+                    if (plus_flag)
+                        count += _putchar('+');
+                    else if (space_flag)
+                        count += _putchar(' ');
+                }
+                count += print_number(num);
+            }
+            else if (*format == 'u')
+                count += print_unsigned(va_arg(args, unsigned int));
+            else if (*format == 'o')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                if (hash_flag && num != 0)
+                {
+                    count += _putchar('0');
+                    count += _putchar('o');
+                }
+                count += print_hex_lower(num);
+            }
+            else if (*format == 'X')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                if (hash_flag && num != 0)
+                {
+                    count += _putchar('0');
+                    count += _putchar('X');
+                }
+                count += print_hex_upper(num);
+            }
+            else if (*format == 'p')
+                count += print_pointer(va_arg(args, void *));
             else
             {
                 _putchar('%');
-                _putchar(*ptr);
+                _putchar(*format);
                 count += 2;
             }
         }
         else
         {
-            _putchar(*ptr);
-            count++;
+            count += _putchar(*format);
         }
-        ptr++;
+
+        format++;
     }
 
+    flash_buffer();
     va_end(args);
-    return count;
+
+    return (count);
 }
