2e39bf9c17cf105b115370fdfa6a2cc2e44d2609
Bader <baxdr001@gmail.com>
2025-03-29 08:36:06 +0000

    betty

--- main.h
+++ main.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #define BUFFER_SIZE 1024
-#define F_MINUS 1  /* Left-align */
-#define F_PLUS 2   /* Show sign */
-#define F_ZERO 4   /* Zero padding */
-#define F_HASH 8   /* Alternate form */
-#define F_SPACE 16 /* Space if no sign */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
 #define S_LONG 2
 #define S_SHORT 1
 /**
@@ -27,7 +27,6 @@ typedef struct fmt_info
 	int size;
 	char specifier;
 } fmt_info_t;
-
 /**
  * struct fmt_handler - Format handler structure
  * @specifier: Format specifier character
@@ -37,9 +36,9 @@ typedef struct fmt_handler
 {
 	char specifier;
 	int (*handler)(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
-}
-fmt_handler_t;
-int write_num(fmt_info_t *fmt, int ind, char buffer[], int length, char padd, char extra_c);
+} fmt_handler_t;
+int write_num(fmt_info_t *fmt, int ind, char buffer[],
+		int length, char padd, char extra_c);
 int _printf(const char *format, ...);
 void init_buffer(char buffer[]);
 void flush_buffer(char buffer[], int *buf_idx);
@@ -55,19 +54,26 @@ int handle_string(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
 int handle_percent(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
 int handle_int(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
 int handle_binary(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
-int handle_unsigned(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
+int handle_unsigned(fmt_info_t *fmt, va_list args,
+		char buffer[], int *buf_idx);
 int handle_octal(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
-int handle_hex_lower(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
-int handle_hex_upper(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
-int handle_hex(fmt_info_t *fmt, va_list args, char map_to[], char buffer[], int *buf_idx);
+int handle_hex_lower(fmt_info_t *fmt, va_list args,
+		char buffer[], int *buf_idx);
+int handle_hex_upper(fmt_info_t *fmt,
+		va_list args, char buffer[], int *buf_idx);
+int handle_hex(fmt_info_t *fmt, va_list args,
+		char map_to[], char buffer[], int *buf_idx);
 int handle_pointer(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
-int handle_nonprint(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
+int handle_nonprint(fmt_info_t *fmt, va_list args,
+		char buffer[], int *buf_idx);
 int handle_reverse(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
 int handle_rot13(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx);
 int write_char(fmt_info_t *fmt, char c, char buffer[], int *buf_idx);
-int write_number(fmt_info_t *fmt, int is_negative, int ind, char buffer[], int *buf_idx);
+int write_number(fmt_info_t *fmt, int is_negative,
+		int ind, char buffer[], int *buf_idx);
 int write_unsigned(fmt_info_t *fmt, int ind, char buffer[], int *buf_idx);
-int write_pointer(fmt_info_t *fmt, char buffer[], int ind, int length, int *buf_idx);
+int write_pointer(fmt_info_t *fmt, char buffer[],
+		int ind, int length, int *buf_idx);
 int is_printable(char c);
 int append_hexa_code(char c, char buffer[], int i);
 int is_digit(char c);
