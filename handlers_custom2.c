a6620d88af8e5a89cdac6716fa3e2455a57663de
Bader <baxdr001@gmail.com>
2025-03-29 07:39:13 +0000

    betty

--- handlers_custom2.c
+++ handlers_custom2.c
@@ -9,34 +9,40 @@
  */
 int handle_rot13(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx)
 {
-	char x, *str, in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *str, x, in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	unsigned int i, j, count = 0, str_len = 0;
+	unsigned int i, j, count = 0, str_len;
+	int width = fmt->width;
 
 	flush_buffer(buffer, buf_idx);
 	str = va_arg(args, char *);
 	if (!str)
 		str = "(AHYY)";
-	for (i = 0; str[i]; i++)
-		str_len++;
-	if (fmt->width > str_len && !(fmt->flags & F_MINUS))
-		for (j = 0; j < fmt->width - str_len; j++)
+
+	for (str_len = 0; str[str_len]; str_len++)
+		;
+
+	if (width > (int)str_len && !(fmt->flags & F_MINUS))
+		for (j = 0; j < (unsigned int)(width - str_len); j++)
 			write(1, (fmt->flags & F_ZERO) ? "0" : " ", 1);
+
 	for (i = 0; str[i]; i++)
 	{
 		for (j = 0; in[j]; j++)
 			if (in[j] == str[i])
 			{
 				x = out[j];
-				write(1, &x, 1), count++;
 				break;
 			}
 		if (!in[j])
-			write(1, &str[i], 1), count++;
+			x = str[i];
+		write(1, &x, 1);
+		count++;
 	}
-	if (fmt->width > count && (fmt->flags & F_MINUS))
-		for (j = 0; j < fmt->width - count; j++)
-			write(1, " ", 1);
-	return ((fmt->width > count) ? fmt->width : count);
-}
 
+	if (width > (int)count && (fmt->flags & F_MINUS))
+		for (j = 0; j < (unsigned int)(width - count); j++)
+			write(1, " ", 1);
+
+	return ((width > (int)count) ? width : count);
+}
