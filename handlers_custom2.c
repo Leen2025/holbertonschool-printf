b60c85b194449ef55f4963849c744513c38e556f
Baxdr <10670@holbertonstudents.com>
2025-03-29 07:41:43 +0000

    betty

--- handlers_custom2.c
+++ handlers_custom2.c
@@ -11,31 +11,31 @@ int handle_rot13(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx)
 {
 	char *str, x, in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	unsigned int i, j, count = 0, str_len;
-	int width = fmt->width;
+	unsigned int i, j, count = 0, str_len = 0;
+	int width;
 
 	flush_buffer(buffer, buf_idx);
 	str = va_arg(args, char *);
 	if (!str)
 		str = "(AHYY)";
 
-	for (str_len = 0; str[str_len]; str_len++)
-		;
+	while (str[str_len])
+		str_len++;
 
+	width = fmt->width;
 	if (width > (int)str_len && !(fmt->flags & F_MINUS))
 		for (j = 0; j < (unsigned int)(width - str_len); j++)
 			write(1, (fmt->flags & F_ZERO) ? "0" : " ", 1);
 
 	for (i = 0; str[i]; i++)
 	{
+		x = str[i];
 		for (j = 0; in[j]; j++)
 			if (in[j] == str[i])
 			{
 				x = out[j];
 				break;
 			}
-		if (!in[j])
-			x = str[i];
 		write(1, &x, 1);
 		count++;
 	}
@@ -44,5 +44,6 @@ int handle_rot13(fmt_info_t *fmt, va_list args, char buffer[], int *buf_idx)
 		for (j = 0; j < (unsigned int)(width - count); j++)
 			write(1, " ", 1);
 
-	return ((width > (int)count) ? width : count);
+	return (width > count ? width : (int)count);
 }
+
