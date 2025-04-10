0d5b62d8837d9cde91e5381801613461fbf54c73
Baxdr <10670@holbertonstudents.com>
2025-04-10 15:27:00 +0000

    update header main

--- main.h
+++ main.h
@@ -21,7 +21,64 @@ typedef struct format
 	char *id;
 	int (*f)();
 } convert_match;
+/**
+ * description: C statements as a pre-processor directive.
+ * LOWX - WIP
+ * UPX - WIP
+ * BUFFERSIZE - local buffer of 1024 chars in order to call write alap
+ * TRUE - boolean flag for non-custom specifiers
+ * FALSE - boolean flag for non-custom specifiers
+ */
 
+#define BUFFERSIZE 1024
+#define TRUE (1 == 1)
+#define FALSE !TRUE
+
+/**
+ * struct stackvar - struct for required custom conversion specifiers
+ * @char1: type char for buffer allocation
+ * @char2: type char for validate n after %
+ * @char3: type char for validate n n after %
+ * @char4: type char for non custom specifiers
+ * @error: type int replaces stdbool.h
+ * @flag: type int for conversion specifiers flags
+ * @space: type int for space printing
+ * @counter: type int for counter between index arrays
+ * @counter_b: type int for counter between buffer and char
+ * @buffer: type char pointer to buffer
+ * @format: type char pointer to format str
+ * @arguments: arguments stacked in va_list
+ * @i: type int that goes over str
+ */
+typedef struct stackvar
+{
+	int i;
+	char char1;
+	char char2;
+	char char3;
+	char char4;
+	int error;
+	int flag;
+	int space;
+	int counter;
+	int counter_b;
+	char *buffer;
+	const char *format;
+	va_list *arguments;
+} stackvar;
+
+/**
+ * struct convert_max_l_h - match the conversion specifiers for non custom
+ * @s: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @func: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct convert_max_l_h
+{
+	char s;
+	void (*func)(stackvar *stack);
+} convert_max_l_h;
 int printf_pointer(va_list val);
 int printf_hex_aux(unsigned long int num);
 int printf_HEX_aux(unsigned int num);
