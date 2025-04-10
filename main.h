89986b4c850b602ccfe4b366f015ad951b5df8fe
Baxdr <10670@holbertonstudents.com>
2025-04-10 15:44:16 +0000

    finish the project

--- main.h
+++ main.h
@@ -1,111 +1,118 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
+#include <stdio.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
 
 /**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
+ * struct fmt - Struct op
  *
+ * @fmt: The format.
+ * @fn: The function associated.
  */
-
-typedef struct format
+struct fmt
 {
-	char *id;
-	int (*f)();
-} convert_match;
-/**
- * description: C statements as a pre-processor directive.
- * LOWX - WIP
- * UPX - WIP
- * BUFFERSIZE - local buffer of 1024 chars in order to call write alap
- * TRUE - boolean flag for non-custom specifiers
- * FALSE - boolean flag for non-custom specifiers
- */
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
 
-#define BUFFERSIZE 1024
-#define TRUE (1 == 1)
-#define FALSE !TRUE
 
 /**
- * struct stackvar - struct for required custom conversion specifiers
- * @char1: type char for buffer allocation
- * @char2: type char for validate n after %
- * @char3: type char for validate n n after %
- * @char4: type char for non custom specifiers
- * @error: type int replaces stdbool.h
- * @flag: type int for conversion specifiers flags
- * @space: type int for space printing
- * @counter: type int for counter between index arrays
- * @counter_b: type int for counter between buffer and char
- * @buffer: type char pointer to buffer
- * @format: type char pointer to format str
- * @arguments: arguments stacked in va_list
- * @i: type int that goes over str
- */
-typedef struct stackvar
-{
-	int i;
-	char char1;
-	char char2;
-	char char3;
-	char char4;
-	int error;
-	int flag;
-	int space;
-	int counter;
-	int counter_b;
-	char *buffer;
-	const char *format;
-	va_list *arguments;
-} stackvar;
-
-/**
- * struct convert_max_l_h - match the conversion specifiers for non custom
- * @s: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @func: type pointer to function for the conversion specifier
+ * typedef struct fmt fmt_t - Struct op
  *
+ * @fmt: The format.
+ * @fm_t: The function associated.
  */
+typedef struct fmt fmt_t;
 
-typedef struct convert_max_l_h
-{
-	char s;
-	void (*func)(stackvar *stack);
-} convert_max_l_h;
-
-stackvar *stackinit(va_list *arguments_list, const char *format);
-void create_buffer(stackvar *stack);
-void place_buffer(stackvar *stack, char *str);
-void ret_buff(char *str, unsigned int n);
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _putchar(char c);
 int _printf(const char *format, ...);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-#endif
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/****************** FUNCTIONS ******************/
+
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* Functions to print numbers */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
+
+/* Function to print non printable characters */
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* Funcion to print memory address */
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+/*Function to print string in reverse*/
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+
+/****************** UTILS ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
+#endif /* MAIN_H */
+
