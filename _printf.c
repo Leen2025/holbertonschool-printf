72d7ae1e49cd4f71c1d8bee5c55cc863a2756b04
Baxdr <10670@holbertonstudents.com>
2025-04-10 13:38:54 +0000

    final fix

--- _printf.c
+++ _printf.c
@@ -1,39 +1,67 @@
 #include "main.h"
-#include <stdarg.h>
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - Produces output according to a format
- * @format: Format string containing format specifiers
- * Return: Number of characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-if (!format)
-return (-1);
-va_start(args, format);
-while (*format)
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
 {
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-count += _putchar(va_arg(args, int));
-else if (*format == 's')
-count += print_string(va_arg(args, char *));
-else if (*format == '%')
-count += _putchar('%');
-else if (*format == 'd' || *format == 'i')
-count += print_number(va_arg(args, int));
-else
-{count += _putchar('%');
-count += _putchar(*format);
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
-}
-else
-count += _putchar(*format);
-format++;
-}
-va_end(args);
-return (count);
-}
+
